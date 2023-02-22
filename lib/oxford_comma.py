98542c3c499f924099ca87b7a220330041c0f13d
Nick Johnson <nlj222@gmail.com>
2023-02-22 14:47:04 -0800

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        print(''.join(items))
+        return ''.join(items)
+    elif len(items) == 2:
+        print(' and '.join(items))
+        return ' and '.join(items)
+    else:
+        names = ', '.join(items[0:-1])
+        names += f", and {items[-1]}"
+        print(names)
+        return names
+
+
+oxford_comma(["the rest"])
+oxford_comma(["Little Marge", "the rest"])
+oxford_comma(["Little Bart", "Little Lisa", "Little Marge", "the rest"])
